3e69776da5734b01eb03cf408b9aaeb62b7e0785
Chapman Siu <chpmn.siu@gmail.com>
2016-05-30 21:13:01 +1000

    fixed all the reasonable missed code coverage checks

--- R/tests/testthat/test_jenks.R
+++ R/tests/testthat/test_jenks.R
@@ -12,3 +12,13 @@ test_that("jenks breaks for iris", {
   expect_gt(min(jenks_breaks), min(iris$Sepal.Length))
   expect_lt(max(jenks_breaks), max(iris$Sepal.Length))
 })
+
+test_that("jenks breaks has control params correctly passed", {
+  breaks_jenks <- create_breaks(1:10, method="jenks", control=list(k=3))
+  expect_length(breaks_jenks, 3)
+})
+
+test_that("jenks breaks has control params correctly passed when k is not in it", {
+  breaks_jenks <- create_breaks(1:10, method="jenks", control=list(subset=NULL))
+  expect_length(breaks_jenks, 3)
+})
