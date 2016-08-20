355ac7e5b081cf4be72b5c78b13a4aa3d6db444d
Chapman Siu <chapm0n.siu@gmail.com>
2016-08-20 11:29:16 +1000

    Update binst.py

--- python/binst/binst.py
+++ python/binst/binst.py
@@ -1,8 +1,9 @@
-import numpy as np
+import pandas as pd
 
 def create_bins(x, breaks):
     """ mirror of the R implementation, create bins given breaks """
     if not isinstance(breaks, list):
         raise Exception('Breaks are not a list')
-    return np.split(x, breaks)
-    
+        
+    breaks = sorted([float("-inf"), float("inf")] + breaks)
+    return pd.cut(x, breaks, labels=range(len(breaks)-1))
